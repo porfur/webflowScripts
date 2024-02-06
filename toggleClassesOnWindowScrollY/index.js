33be83d4f1c4cd4e4e9cc724832c4c714bc4bcb0
OP <ovidiu.ps@gmail.com>
2024-02-06 12:18:39 +0200

    Added toggle Classes on scrollY

---
+++ toggleClassesOnWindowScrollY/index.js
@@ -0,0 +1,63 @@
+const opToggleClassesOnWindowScroll = (() => {
+  function setup() {
+    const scrollClass = "op-scroll__class";
+    const scrollOffset = "op-scroll__offset";
+    const scrollChild = "op-scroll__child";
+    const targets = document.querySelectorAll(`[${scrollClass}]`);
+
+    targets.forEach((target) => {
+      const childScrollClasses = getChildrenScrollClasses(target, scrollChild);
+
+      addCustomAttributeToChildren(target, childScrollClasses, scrollClass);
+
+      window.addEventListener("scroll", () => {
+        if (window.scrollY > getScrollOffset(target, scrollOffset)) {
+          activateNodeAndChildren(target, scrollClass);
+        } else {
+          deactivateNodeAndChildren(target, scrollClass);
+        }
+      });
+    });
+  }
+
+  //Helpers
+  function getScrollOffset(parentNode, scrollOffset) {
+    let targetOffset = parseInt(parentNode.getAttribute(scrollOffset));
+    return isNaN(targetOffset) ? 0 : targetOffset;
+  }
+
+  function activateNodeAndChildren(parentNode, attrName) {
+    parentNode.classList.add(parentNode.getAttribute(attrName));
+    parentNode
+      .querySelectorAll(`[${attrName}]`)
+      .forEach((child) => child.classList.add(child.getAttribute(attrName)));
+  }
+
+  function deactivateNodeAndChildren(parentNode, attrName) {
+    parentNode.classList.remove(parentNode.getAttribute(attrName));
+    parentNode
+      .querySelectorAll(`[${attrName}]`)
+      .forEach((child) => child.classList.remove(child.getAttribute(attrName)));
+  }
+
+  function addCustomAttributeToNodes(nodes, attrName, attrValue) {
+    nodes.forEach((node) => node.setAttribute(attrName, attrValue));
+  }
+
+  function addCustomAttributeToChildren(parent, tupleArr, attrName) {
+    tupleArr.forEach(([selector, className]) => {
+      const childNodes = parent.querySelectorAll(selector);
+      addCustomAttributeToNodes(childNodes, attrName, className);
+    });
+  }
+
+  function getChildrenScrollClasses(parent, identifierAttr) {
+    return Array.from(parent.attributes)
+      .filter((attr) => attr.name.includes(identifierAttr))
+      .map((attr) => attr.value.split(":").map((str) => str.trim()));
+  }
+
+  return setup;
+})();
+
+document.addEventListener("DOMContentLoaded", opToggleClassesOnWindowScroll);
