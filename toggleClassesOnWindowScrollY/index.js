623ccb1c20b94233ecf1b2ca494e40901f5dda05
OP <ovidiu.ps@gmail.com>
2024-02-19 10:06:44 +0200

    Add optional Scroll conditions

--- toggleClassesOnWindowScrollY/index.js
+++ toggleClassesOnWindowScrollY/index.js
@@ -3,30 +3,73 @@ const opToggleClassesOnWindowScroll = (() => {
     const scrollClass = "op-y-scroll__class";
     const scrollOffset = "op-y-scroll__offset";
     const scrollChild = "op-y-scroll__child";
+    const scrollCondition = "op-y-scroll__condition";
+    const scrollConditionType = "op-y-scroll__cond-type";
+    const every = "every"; // Default
+    const some = "some";
+    const getConditionResult = {
+      [every]: getEveryConditionResult,
+      [some]: getSomeConditionResult,
+    };
     const targets = document.querySelectorAll(`[${scrollClass}]`);
 
     targets.forEach((target) => {
-      const childrenAndClasses = getChildrenAndClasses(target, scrollChild);
-
+      const childrenAndClasses = getKeyValuePairs(target, scrollChild);
       addCustomAttributeToChildren(target, childrenAndClasses, scrollClass);
+      const conditions = getKeyValuePairs(target, scrollCondition);
 
-      let isActive = false;
-      window.addEventListener("scroll", () => {
-        const offset = getScrollOffset(target, scrollOffset);
-        const passedOffset = window.scrollY > offset;
-
-        if (passedOffset && !isActive) {
-          activateNodeAndChildren(target, scrollClass);
-          isActive=true
-        } else if (!passedOffset && isActive) {
-          deactivateNodeAndChildren(target, scrollClass);
-          isActive = false;
-        }
-      });
+      if (conditions.length === 0) {
+        addListenerOnScroll(scrollClass, scrollOffset);
+        return;
+      }
+      const conditionType = getConditionType(
+        target,
+        scrollConditionType,
+        every,
+      );
+      getConditionResult[conditionType](conditions) &&
+        addListenerOnScroll(scrollClass, scrollOffset);
     });
   }
 
   //Helpers
+  function addListenerOnScroll(scrollClass, scrollOffset) {
+    let isActive = false;
+    window.addEventListener("scroll", () => {
+      const offset = getScrollOffset(target, scrollOffset);
+      const passedOffset = window.scrollY > offset;
+
+      if (passedOffset && !isActive) {
+        activateNodeAndChildren(target, scrollClass);
+        isActive = true;
+      } else if (!passedOffset && isActive) {
+        deactivateNodeAndChildren(target, scrollClass);
+        isActive = false;
+      }
+    });
+  }
+
+  function getSomeConditionResult(conditions) {
+    return conditions.some(([selector, className]) =>
+      document
+        .querySelectorAll(selector)
+        .some((element) => element.classList.contains(className)),
+    );
+  }
+  function getEveryConditionResult(conditions) {
+    return conditions.every(([selector, className]) =>
+      document
+        .querySelectorAll(selector)
+        .every((element) => element.classList.contains(className)),
+    );
+  }
+
+  function getConditionType(target, scrollConditionType, defaultCondition) {
+    return target.hasAttribute(scrollConditionType)
+      ? target.getAttribute(scrollConditionType)
+      : defaultCondition;
+  }
+
   function getScrollOffset(parentNode, scrollOffset) {
     let targetOffset = parseInt(parentNode.getAttribute(scrollOffset));
     return isNaN(targetOffset) ? 0 : targetOffset;
@@ -57,7 +100,7 @@ const opToggleClassesOnWindowScroll = (() => {
     });
   }
 
-  function getChildrenAndClasses(parent, identifierAttr) {
+  function getKeyValuePairs(parent, identifierAttr) {
     return Array.from(parent.attributes)
       .filter((attr) => attr.name.includes(identifierAttr))
       .map((attr) => attr.value.split(":").map((str) => str.trim()));
